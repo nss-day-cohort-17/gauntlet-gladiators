7e36c39578e1565f437d3ed9ca4be24be6274e18
John McCutchan <john.mccutchan.wd@gmail.com>
2016-12-14 15:31:23 -0600

    added crude combat

--- javascripts/combat.js
+++ javascripts/combat.js
@@ -1,5 +1,22 @@
-function combat(){
-  var aStrength = player.strength + player.strengthBonus;
-  var aHealth = player.health + player.healthBonus;
-  console.log(); //**** Meassage *******************************
+var aStrength = 0;
+var aHealth = 0;
+var outputPlayer ="";
+
+$("#attack").click(function(e){
+    console.log("Button clicked")
+    aStrength = Number(player.strength);
+    console.log("Player strength: ", player.strength);
+    aHealth = Number(player.health);
+    console.log("Player health: ", aHealth, player.playerName);; //**** Meassage *******************************
+    displayCombat();
+})
+
+
+function displayCombat(e) {
+  console.log("Player Name: ", player.name);
+  outputPlayer = `<h2>${player.playerName}</h2>
+                  <h3>${aStrength}</h3>
+                  <h3>${aHealth}</h3>
+                 `
+  document.getElementById("playerHere").innerHTML = outputPlayer;
 }

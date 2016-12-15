6fb060b5a7b7f9f9615e3a3fd7fcc80a96d6baf3
John McCutchan <john.mccutchan.wd@gmail.com>
2016-12-15 11:52:18 -0600

    worked combat loop and initial display call

--- javascripts/combat.js
+++ javascripts/combat.js
@@ -1,26 +1,69 @@
+// Global Variables
 var aStrength = 0;
 var aHealth = 0;
 var outputPlayer ="";
 var bStrength = 0;
 var bHealth = 0;
 var outputEnemy ="";
-
-$("#attack").click(function(e){
+// Listeners
+document.getElementById("loadIntro").addEventListener("click", loadDisplay);
+document.getElementById("attack").addEventListener("click", attackNow);
+//
+/*==============================================*/
+/* Display ======================================*/
+/*==============================================*/
+function loadDisplay(){
 // Player Render
-    aStrength = Number(player.strength);
-    aHealth = Number(player.health);
-    outputPlayer += `<h2>${player.playerName}</h2>
+  aStrength = player.strength + player.weapon.damage;
+  aHealth = player.health + player.class.healthBonus;
+  outputPlayer = `<h2>${player.playerName}</h2>
                   <h3>Strength: ${aStrength}</h3>
                   <h3>Health: ${aHealth}</h3>
                  `
-    document.getElementById("playerHere").innerHTML = outputPlayer;
+  document.getElementById("playerHere").innerHTML = outputPlayer;
 // Enemy Render
-    console.log("Enemy Name: ", enemy.name);
-    outputEnemy += `<h2>${enemy.playerName}</h2>
+  enemy.name = enemy.species;
+  bStrength = enemy.strength + enemy.weapon.damage;
+  bHealth = enemy.health + enemy.class.healthBonus;
+  console.log("Enemy Health: ", bHealth);
+  outputEnemy = `<h2>${enemy.species}</h2>
+                 <h3>Strength: ${bStrength}</h3>
+                 <h3>Health: ${bHealth}</h3>
+                `
+  document.getElementById("enyHere").innerHTML = outputEnemy;
+  var x = "";
+  var y = player.playerName;
+  var z = enemy.species;
+  x = `<p>${y} is facing a ${z}!</p>`
+  document.getElementById("intro").innerHTML = x;
+}
+
+// <p>${player.toString()} is facing ${enemy.toString()}</p>
+//
+/*==============================================*/
+/* Attack ======================================*/
+/*==============================================*/
+function attackNow(e) {
+console.log("Attack Clicked");
+// Call the Combat!
+   // displayCombat();
+}
+//
+/*==============================================*/
+/* Display Combat ==============================*/
+/*==============================================*/
+function displayCombat(e){
+  console.log("Health: ", bHealth);
+  bHealth = bHealth - 5;
+  console.log("Health: ", bHealth);
+  outputEnemy = `<h2>${enemy.species}</h2>
                   <h3>Strength: ${bStrength}</h3>
                   <h3>Health: ${bHealth}</h3>
                  `
-    document.getElementById("enyHere").innerHTML = outputPlayer;
-// Call the Combat!
-   displayCombat();
-})
+  document.getElementById("enyHere").innerHTML = outputEnemy;
+
+
+}
+
+
+// TODO: If statement, magical: use magic, not: use weapon.

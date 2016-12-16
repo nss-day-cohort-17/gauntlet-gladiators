32fd72fa16d011f84f3cb23192c27ff6b332bbf3
John McCutchan <john.mccutchan.wd@gmail.com>
2016-12-16 09:20:51 -0600

    updated combat loop

--- javascripts/combat.js
+++ javascripts/combat.js
@@ -48,11 +48,22 @@ function loadDisplay(){
   bStrength = enemy.strength + enemy.weapon.damage;
   bHealth = enemy.health + enemy.class.healthBonus;
   console.log("Enemy Health: ", bHealth);
-  outputEnemy = `<h2>${enemy.species}</h2>
-                 <h3>Strength: ${bStrength}</h3>
-                 <h3>Health: ${bHealth}</h3>
-                `
-  document.getElementById("enyHere").innerHTML = outputEnemy;
+  if(enemy.class.magical === true){
+    bPower = enemy.intelligence + enemy.spell.damage;
+    outputEnemy = `<h2>${enemy.playerName}</h2>
+                    <h3>Power: ${bPower}</h3>
+                    <h3>Health: ${bHealth}</h3>
+                    `
+    document.getElementById("enyHere").innerHTML = outputEnemy;
+    console.log("Magical? ",enemy.spell.damage)
+  } else {
+    aStrength = enemy.strength + enemy.weapon.damage;
+    outputEnemy = `<h2>${enemy.species}</h2>
+                    <h3>Strength: ${bStrength}</h3>
+                    <h3>Health: ${bHealth}</h3>
+                 `
+    document.getElementById("enyHere").innerHTML = outputEnemy;
+  }
 }
 //
 /*==============================================*/
@@ -60,18 +71,51 @@ function loadDisplay(){
 /*==============================================*/
 function attackNow(e) {
 console.log("Attack Clicked");
-// if(player.)
-
-
+// Player Attacks Enemy
+  if(player.class.magical === true){
+    bHealth = bHealth - aPower;
+    console.log("After hit", bHealth);
+  }else{
+    bHealth = bHealth - aStrength;
+  }
+  if(bHealth <= 0){
+    outputEnemy =`<h2>${enemy.species}</h2>
+                  <h3>is dead.</h3>`
+                  endGame();
+    document.getElementById("enyHere").innerHTML = outputEnemy;
+    var x = `<h3 class="center">${enemy.species} is dead!</h3>`
+    document.querySelector("#intro").innerHTML = x;
+  }else{
+    outputEnemy = `<h2>${enemy.species}</h2>
+                  <h3>Health: ${bHealth}</h3>`
+    document.getElementById("enyHere").innerHTML = outputEnemy;
+  }
+// Enemy Attacks Player
+  if(enemy.class.magical === true){
+    aHealth = aHealth - bPower;
+    console.log("After hit", aHealth);
+  }else{
+    aHealth = aHealth - bStrength;
+  }
+  if(aHealth <= 0){
+    outputPlayer =`<h2>${player.playerName}</h2>
+                    <h3>Is dead.</h3>`
+                    endGame();
+    document.getElementById("playerHere").innerHTML = outputPlayer;
+    var x = `<h3 class="center">${player.playerName} is dead!</h3>`
+    document.querySelector("#intro").innerHTML = x;
+  }else{
+    outputPlayer =`<h2>${player.playerName}</h2>
+                   <h3>Health: ${aHealth}</h3>`
+    document.getElementById("playerHere").innerHTML = outputPlayer;
+  }
 }
-// TODO: If statement, magical: use magic, not: use weapon.
-
-
-
-
-
-
-
-
-
+/*==============================================*/
+/* End Game ====================================*/
+/*==============================================*/
+function endGame(){
+  console.log("End Game");
+  var clearz = document.getElementById("attack");
+  clearz.classList.add("clear");
+}
 //
